0959a7942adb9bc4c0902291d71cbc786a940909
Анжелика <anhamenkova@gmail.com>
2026-05-11 00:32:40 +0300

    тоже самое что и в ListSequence но только о что не изменяет обьект

---
+++ include/immutable_list_sequence.hpp
@@ -0,0 +1,76 @@
+#pragma once
+#include "exceptions.hpp"
+#include "linked_list.hpp"
+#include "sequence.hpp"
+
+template <typename T>
+class ImmutableListSequence : public Sequence<T>
+{
+   private:
+    LinkedList<T>* items;
+
+   public:
+    ImmutableListSequence()
+    {
+        items = new LinkedList<T>();
+    }
+
+    template <size_t N>
+    ImmutableListSequence(T (&arr)[N])
+    {
+        items = new LinkedList<T>(arr);
+    }
+
+    ImmutableListSequence(const ImmutableListSequence<T>& other)
+    {
+        items = new LinkedList<T>(*other.items);
+    }
+
+    ~ImmutableListSequence()
+    {
+        delete items;
+    }
+
+    T GetFirst() const override
+    {
+        if (items->GetLength() == 0)
+        {
+            throw IndexOutOfRange{};
+        }
+        return items->GetFirst();
+    }
+
+    T GetLast() const override
+    {
+        if (items->GetLength() == 0)
+        {
+            throw IndexOutOfRange{};
+        }
+        return items->GetLast();
+    }
+
+    T Get(size_t index) const override
+    {
+        return items->Get(index);
+    }
+
+    size_t GetLength() const override
+    {
+        return items->GetLength();
+    }
+
+    Sequence<T>* GetSubsequence(size_t startIndex, size_t endIndex) const override
+    {
+        ImmutableListSequence<T>* result = new ImmutableListSequence<T>();
+        for (size_t i = startIndex; i <= endIndex; i++)
+        {
+            result->items->Append(items->Get(i));
+        }
+        return result;
+    }
+
+    const T& operator[](size_t index) const
+    {
+        return (*items)[index];
+    }
+};
